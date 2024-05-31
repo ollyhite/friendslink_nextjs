9c9b8fe347e364a134ad003eca1e386c312c09e3
PeiYun <ollylee8520@gmail.com>
2024-05-31 11:17:47 -0600

    update header, layout and about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+    </div>
+  );
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center mx-2 max-w-6xl sm:mx-auto py-6">
+      <div className="flex ">
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold py-1 px-2 rounded-lg mr-1">
+              FriendsLink
+            </span>
+          </h2>
+        </Link>
+      </div>
+      <div className="flex items-center space-x-5">
+        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
+        {/* <DarkModeSwitch /> */}
+      </div>
+    </div>
+  );
+}
